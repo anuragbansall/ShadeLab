3b6c49e352a694149e6b751407146a6b63f3ca07
Anurag Bansal <devanuragbansal@gmail.com>
2024-10-29 23:59:58 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+function getColorPallate(hex, mode) {
+    return fetch(`https://www.thecolorapi.com/scheme?hex=${hex}&mode=${mode}`)
+        .then(res => res.json())
+        .then(data => {
+            const colorPallate = data.colors;
+            const colorPallateHtml = colorPallate.map(color => `
+                <div class="color" data-hex="${color.hex.value}">
+                    <div class="color-fill" style="background-color: ${color.hex.value}"></div>
+                    <p>${color.hex.value}</p>
+                </div>
+            `).join('');
+            document.getElementById('color-pallate').innerHTML = colorPallateHtml;
+
+            // Add click event listener to the newly created color elements
+            document.querySelectorAll('.color').forEach(colorElement => {
+                colorElement.addEventListener('click', function() {
+                    const hexValue = this.getAttribute('data-hex');
+                    copyToClipboard(hexValue);
+                });
+            });
+        });
+}
+
+document.getElementById('generate-button').addEventListener('click', function() {
+    const hex = document.getElementById('color-selector').value.replace('#', '');
+    const mode = document.getElementById('mode-selector').value;
+
+    getColorPallate(hex, mode);
+});
+
+function copyToClipboard(hexValue) {
+    navigator.clipboard.writeText(hexValue)
+        .then(() => {
+            const popup = document.getElementById('copy-color-popup')
+            popup.textContent = `${hexValue} copied to clipboard!`
+            popup.style.visibility = 'visible'
+            setTimeout(() => {
+                popup.style.visibility = 'hidden'
+            }, 2000);
+        })
+        .catch(err => {
+            console.error('Could not copy text: ', err);
+        });
+}
